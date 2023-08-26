09acf18ec3243eca8fa11e035dfe21e601e41ba6
Touqeer Ansari <touqeeransari001@gmail.com>
2023-08-26 13:31:10 +0530

    initial commits

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
-// import logo from './logo.svg';
+
+
+
 import './App.css';
 
 //components
@@ -6,19 +8,19 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home'
 import {Box} from '@mui/material';
 
-import DataProvider from './Context/DataProvider'
+import  ContextProvider from './Context/DataProvider';
 
 
 
 function App() {
   return (
+   <ContextProvider>
 
-  <DataProvider>
-    
   <Header/>
   <Box style={{marginTop:54}}/>
   <Home/>
-</DataProvider>
+  </ContextProvider>
+ 
   );
 }
 
