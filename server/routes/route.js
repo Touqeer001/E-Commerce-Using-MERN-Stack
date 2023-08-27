8fee82f195306855e1042a00c4b0a412c46ca403
Touqeer Ansari <touqeeransari001@gmail.com>
2023-08-27 22:27:01 +0530

    all routes

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,8 +1,11 @@
 import express from 'express'
-import { userSignUp } from '../controller/user-controller.js';
+import { userSignUp,userLogin} from '../controller/user-controller.js';
 const router=express.Router();
 
 // router.post('/signup',userSignUp);
 router.post('/signup', userSignUp);
+
+//FOR LOGIN
+router.post('/login',userLogin)
 
 export default router;
