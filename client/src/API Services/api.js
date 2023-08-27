26dd55556e53793bfa215f0b4401ba1babb0e412
Touqeer Ansari <touqeeransari001@gmail.com>
2023-08-27 22:24:20 +0530

    all api service

--- client/src/API Services/api.js
+++ client/src/API Services/api.js
@@ -4,19 +4,28 @@ import axios from 'axios';
 const url='http://localhost:8000';
 
 
+// the function authenticateLogin is designed to make an asynchronous POST request to a login API endpoint using Axios. 
+//If the request is successful, it returns the promise that Axios returns. If there's an error during the request, 
+//the error is caught and logged to the console. The function can be used elsewhere in your code to handle authentication/login requests.
 export const authenticateLogin = async (data) => {
     try {
         return await axios.post(`${url}/login`, data)
     } catch (error) {
         console.log('Error while calling login API: ', error);
+        return error.response;
     }
 }
-
+//api for signup user
 
 export const authenticateSignup = async (data) => {
     try {
         return await axios.post(`${url}/signup`, data)
     } catch (error) {
         console.log('Error while calling Signup API: ', error);
+ 
     }
 }
+
+
+
+
