bb5f5f265fbd314b15ca404e815cbca295f71bb4
Touqeer Ansari <touqeeransari001@gmail.com>
2023-09-04 21:48:55 +0530

    adding products api

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,11 +1,18 @@
 import express from 'express'
 import { userSignUp,userLogin} from '../controller/user-controller.js';
-const router=express.Router();
+import { getProducts } from '../controller/product-controller.js';
 
+
+const router=express.Router();
 // router.post('/signup',userSignUp);
 router.post('/signup', userSignUp);
 
 //FOR LOGIN
 router.post('/login',userLogin)
+
+//getting product detail from the front end..getProducts return the all products from the
+router.get('/products',getProducts)
+
+
 
 export default router;
