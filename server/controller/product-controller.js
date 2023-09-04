9138fe491f7d793ce3d6ed5a945aec52fe1fa5f0
Touqeer Ansari <touqeeransari001@gmail.com>
2023-09-04 21:50:47 +0530

    retrive product from databse

---
+++ server/controller/product-controller.js
@@ -0,0 +1,18 @@
+
+//"getProducts" is an asynchronous function that accepts two parameters: request and response. These parameters are likely related to handling HTTP requests and responses in a web server or API context.
+
+//Inside the try block:
+// It attempts to retrieve all products from the imported products model using the find method. This suggests that products is likely a database model or an object representing a collection of products.
+// Once the products are fetched, it responds with an HTTP status of 200 (OK) and sends the fetched products as JSON in the response.
+
+
+import products from "../Model/Product-Schema.js";
+
+export const getProducts = async (request, response) => {
+  try {
+    const product = await products.find({});
+    response.status(200).json(product);
+  } catch (error) {
+    response.status(500).json({ messege: error.messege });
+  }
+};
