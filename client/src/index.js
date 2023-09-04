a41449b81d07fc7a7fd55c638dcb9d0d60ba2def
Touqeer Ansari <touqeeransari001@gmail.com>
2023-09-04 21:52:15 +0530

    adding redux to index

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,16 @@
-
- 
- 
+import React from 'react';
+import reportWebVitals from './reportWebVitals';
+import store from './components/redux/store.js';
 import App from './App'; 
+import { Provider } from 'react-redux';
 
 import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render( 
+    <Provider store={store} >
+    <React.StrictMode>
+<App />
+</React.StrictMode>
+</Provider>
+);
+reportWebVitals();
