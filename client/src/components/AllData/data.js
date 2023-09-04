d0a83553f46d93284b9f47f3d3c283af331f8cbf
Touqeer Ansari <touqeeransari001@gmail.com>
2023-09-04 21:52:34 +0530

    navbaar data

--- client/src/components/AllData/data.js
+++ client/src/components/AllData/data.js
@@ -18,6 +18,8 @@ export const navData = [
 
 export const bannerData = [
     { id: 1, url: 'https://rukminim1.flixcart.com/flap/3376/560/image/d117a62eb5fbb8e1.jpg?q=50' },
+    // { id: 1, url: 'https://rukminim1.flixcart.com/image/3376/560/k3uhhu80/hair-dryer/n/m/t/nova-2800-w-professional-nhp-8220-original-imafmvwfhmzsxdrw.jpeg?q=20'},
+  
     { id: 2, url: 'https://rukminim1.flixcart.com/flap/3376/560/image/57267a180af306fe.jpg?q=50' },
     { id: 3, url: 'https://rukminim1.flixcart.com/flap/3376/560/image/ae9966569097a8b7.jpg?q=50' },
     { id: 4, url: 'https://rukminim1.flixcart.com/flap/3376/560/image/f6202f13b6f89b03.jpg?q=50' }
