7c2c0e4a01a474400d682688da400a61366ec922
Touqeer Ansari <touqeeransari001@gmail.com>
2023-09-06 03:11:40 +0530

    all about product

--- client/src/components/redux/action/productAction.js
+++ client/src/components/redux/action/productAction.js
@@ -16,7 +16,18 @@ export const getProducts=()=>async(dispatch)=>{
 //    console.log("Error  While calling getProducts Api",error.message);
 dispatch({type:actionTypes.GET_PRODUCTS_FAIL, payload: error.message})
     }
-    
-
-    
+   
 }
+export const getProductDetails = (id) =>async(dispatch) => {
+    try {
+     
+        const {data} = await axios.get(`${URL}/product/${id}`);
+        dispatch({ type: actionTypes.GET_PRODUCT_DETAILS_REQUEST });
+        
+        dispatch({ type: actionTypes.GET_PRODUCT_DETAILS_SUCCESS, payload: data });
+
+    } catch (error) {
+        dispatch({ type: actionTypes.GET_PRODUCT_DETAILS_FAIL, payload: error.response});
+
+    }
+};
