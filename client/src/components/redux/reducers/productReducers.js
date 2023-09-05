1a7ef1c6080fc8b1c26011c13b5b3546737ca69a
Touqeer Ansari <touqeeransari001@gmail.com>
2023-09-06 03:09:55 +0530

    adding more functiion

--- client/src/components/redux/reducers/productReducers.js
+++ client/src/components/redux/reducers/productReducers.js
@@ -16,3 +16,25 @@ export const getProductReducer= (state = {products: []}, action) => {
             return state
     }
 };
+
+
+export const getProductDetailsReducer = (state = {product:{}}, action) => {
+    
+    switch(action.type){
+        case actionTypes.GET_PRODUCT_DETAILS_REQUEST:
+            return { loading: true }
+        case actionTypes.GET_PRODUCT_DETAILS_SUCCESS:
+            return { loading: false, product: action.payload }
+        case actionTypes.GET_PRODUCT_DETAILS_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+        case actionTypes.GET_PRODUCT_DETAILS_RESET: 
+            return {
+                product: {}
+            }
+        default:
+            return state
+    }
+}
