dea6d4998d594166085418ce5cfe7c4526bf578b
Touqeer Ansari <touqeeransari001@gmail.com>
2023-09-06 03:17:59 +0530

    redux store

--- client/src/components/redux/store.js
+++ client/src/components/redux/store.js
@@ -2,16 +2,21 @@
 // Redux is an open-source JavaScript library used primarily for managing the state of a web application. It is often employed in conjunction with React, though it can be used with other JavaScript frameworks or libraries as well. Redux follows the principles of a predictable state container and is frequently used in complex applications to maintain a centralized and predictable state management system.
 
 
-import {getProductReducer} from './reducers/productReducers.js'
+import {getProductDetailsReducer, getProductReducer} from './reducers/productReducers.js'
 import {createStore,combineReducers,applyMiddleware} from 'redux'
 import{composeWithDevTools} from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
+import { cartReducer } from './reducers/CardReducer.js';
 
 const reducer=combineReducers({
-    getProducts:getProductReducer
+    cart: cartReducer,
+    getProducts:getProductReducer,
+    getProductDetails:getProductDetailsReducer
+
 })
 
+console.log(getProductDetailsReducer)
 
 const middleware = [thunk];
 
