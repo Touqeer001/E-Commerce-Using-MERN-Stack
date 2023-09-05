72a54f7e7e8f9d2c0a3641a4c7dfabb04fcac405
Touqeer Ansari <touqeeransari001@gmail.com>
2023-09-06 03:15:46 +0530

    header componets

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,6 +1,10 @@
-import {AppBar,Toolbar,styled,Box} from '@mui/material';
+
+import { useState } from 'react';
+import {AppBar,Toolbar,styled,Box, IconButton, Drawer,List} from '@mui/material';
 import Search from './Search';
 import CustomButtons from './CustomButtons';
+import { Menu } from '@mui/icons-material';
+
 
 const StyledHeader=styled(AppBar)`
 // background:#2874f0;
@@ -9,23 +13,65 @@ background:black;
 height:50px;
 
 
-
-
 `
+const CustomButtonWrapper = styled('span')(({ theme }) => ({ 
+    margin: '0 5% 0 auto', 
+    [theme.breakpoints.down('sm')]: {
+        display: 'none'
+    }
+}));
+const MenuButton = styled(IconButton)(({ theme }) => ({
+    display: 'none',
+
+    [theme.breakpoints.down('sm')]: {
+        display: 'block',
+        background: 'white',
+        color:'black',
+       // borderRadius: 'none',
+        marginRight: '30px',
+    }
+}));
 
 
 
 const Header=()=>{
+    const [open, setOpen] = useState(false);
+   
+    const handleClose = () => {
+        setOpen(false);
+    }
+    const handleOpen = () => {
+        setOpen(true);
+    }
 
+
+    
+    const list = () => (
+        <Box style={{ width: 250 }} onClick={handleClose}>
+            <List>
+                <listItem button>
+                    <CustomButtons />
+                </listItem>
+            </List>
+        </Box>
+    );
     return(
-        <StyledHeader>
-            <Toolbar>
-                <Box>
-                    {/* <img src="" alt="Image Not Found"></img> */}
-
-                </Box>
+        <StyledHeader  position="fixed">
+            <Toolbar style={{ minHeight: 55 }}>
+            <MenuButton
+                    color="inherit"
+                     onClick={handleOpen}
+                >
+                    <Menu />
+                </MenuButton>
+                <Drawer open={open} onClose={handleClose}>
+                     {list()}
+                </Drawer>
                 <Search/>
+                <CustomButtonWrapper>
+
                 <CustomButtons/>
+                </CustomButtonWrapper>
 
             </Toolbar>
        
