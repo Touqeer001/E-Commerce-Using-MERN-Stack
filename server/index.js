3b81affa5e303f8d0b68425960b580e84e2e42e8
Touqeer Ansari <touqeeransari001@gmail.com>
2023-09-06 23:50:28 +0530

    initial commits

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import express from "express";
 import Router from './routes/route.js'
 import cors from 'cors'
 import bodyParser from "body-parser";
+import { v4 as uuid } from 'uuid';
 
 const app = express();
 dotenv.config();
@@ -16,10 +17,24 @@ app.use('/',Router);
 
 
 
-const PORT = 8000;
+const PORT = process.env.PORT // 8000;
 const username=process.env.DB_USERNAME;
 const password=process.env.DB_PASSWORD;
 Connection(username,password);
 
 app.listen(PORT, () => console.log("Server is Running successfully on 8000 Port...."));
 DefaultData();
+
+
+export let paytmMerchantkey = process.env.PAYTM_MERCHANT_KEY;
+export let paytmParams = {};
+paytmParams['MID'] = process.env.PAYTM_MID,
+paytmParams['WEBSITE'] = process.env.PAYTM_WEBSITE,
+paytmParams['CHANNEL_ID'] = process.env.PAYTM_CHANNEL_ID,
+paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID,
+paytmParams['ORDER_ID'] = uuid(),
+paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID,
+paytmParams['TXN_AMOUNT'] = '100',
+paytmParams['CALLBACK_URL'] = 'http://localhost:8000/callback'
+paytmParams['EMAIL'] = 'kunaltyagi@gmail.com'
+paytmParams['MOBILE_NO'] = '1234567852'
