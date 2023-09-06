2c20bca97ad2db0a5f26861599909faa546bbc5d
Touqeer Ansari <touqeeransari001@gmail.com>
2023-09-06 23:44:07 +0530

    adding Router

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { userSignUp,userLogin} from '../controller/user-controller.js';
 import { getProducts,getProductById} from '../controller/product-controller.js';
+import { addPaymentGateway, paymentResponse} from '../controller/payment-controller.js'
 
 
 const router=express.Router();
@@ -14,6 +15,8 @@ router.post('/login',userLogin)
 router.get('/products',getProducts)
 router.get('/product/:id', getProductById);
 
+router.post('/payment', addPaymentGateway);
+router.post('/callback', paymentResponse);
 
 
 export default router;
