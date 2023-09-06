14ad5f0d987c29340637ebd523cf7c39338eec80
Touqeer Ansari <touqeeransari001@gmail.com>
2023-09-06 23:48:07 +0530

    cart componets

--- client/src/components/redux/action/cardAction.js
+++ client/src/components/redux/action/cardAction.js
@@ -4,7 +4,7 @@ import * as actionTypes from '../constants/cartConstant.js'
 import axios from 'axios';
 export const addToCart = (id, quantity) => async (dispatch) => {
     try { 
-        const { data } = await axios.get(`http://localhost:8000/product/${id}`);
+        const {data } = await axios.get(`http://localhost:8000/product/${id}`);
 
         dispatch({ type: actionTypes.ADD_TO_CART, payload: { ...data, quantity } });
 
